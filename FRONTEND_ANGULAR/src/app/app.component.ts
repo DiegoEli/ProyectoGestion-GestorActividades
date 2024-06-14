5cc10a81cb89302c934fd2db404025b58ffb3b6f
DiegoEli <diego.mendozam@ug.edu.ec>
2024-06-13 22:10:00 -0500

    actualizar pantalla login

--- FRONTEND_ANGULAR/src/app/app.component.ts
+++ FRONTEND_ANGULAR/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
-  title = 'FRONTEND_ANGULAR';
+    title = 'FRONTEND_ANGULAR';
 }
