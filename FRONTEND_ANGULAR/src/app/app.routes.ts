1eea6da33f5f56c0bf535b48cec3407ad20c1722
Hidalgo1996 <GeForce210_1996@hotmail.com>
2024-06-23 22:14:29 -0500

    Configuracion de ruta Home

--- FRONTEND_ANGULAR/src/app/app.routes.ts
+++ FRONTEND_ANGULAR/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
     { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
